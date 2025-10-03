1c1b2c75c9d4a956f799de19b13a87c9e7394df6
[Niggle Nandu] <nigglenandu@gmail.com>
2025-10-03 23:50:39 +0545

    studentclient cmnt

--- OpenFeign/School-Service/src/main/java/MicroServicesEg/School_Service/clients/StudentClient.java
+++ OpenFeign/School-Service/src/main/java/MicroServicesEg/School_Service/clients/StudentClient.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+// Feign client → helps one microservice (School-Service) call another (Student-Service)
+// 'name' = service identifier, 'url' = base URL of student-service
 @FeignClient(name = "student-service", url = "http://localhost:8082")
 public interface StudentClient {
+
+    // Call GET API of Student-Service → fetch student by ID
     @GetMapping("/student/{id}")
     StudentDto getStudentById(@PathVariable("id") Long id);
 
+    // Call POST API of Student-Service → create a new student
     @PostMapping("/student/create")
     ResponseEntity<String> createStudent(@RequestBody StudentDto student);
 }
