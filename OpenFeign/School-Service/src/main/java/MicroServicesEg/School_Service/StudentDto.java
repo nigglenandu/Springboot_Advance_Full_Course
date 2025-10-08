a6387bca540840b12366b616b5c3fb01b3345ffd
[Niggle Nandu] <nigglenandu@gmail.com>
2025-10-08 23:39:38 +0545

    student dto cmnt

--- OpenFeign/School-Service/src/main/java/MicroServicesEg/School_Service/StudentDto.java
+++ OpenFeign/School-Service/src/main/java/MicroServicesEg/School_Service/StudentDto.java
@@ -1,11 +1,22 @@
 package MicroServicesEg.School_Service;
 
+// This is a Data Transfer Object (DTO) for Student
+// DTOs are used to transfer data between services or layers
 public class StudentDto {
+
+    // Unique identifier for the student
     private long id;
+
+    // Name of the student
     private String name;
+
+    // Class/grade of the student
     private String className;
+
+    // Age of the student
     private int age;
 
+    // Getter and Setter for age
     public int getAge() {
         return age;
     }
@@ -14,6 +25,7 @@ public class StudentDto {
         this.age = age;
     }
 
+    // Getter and Setter for className
     public String getClassName() {
         return className;
     }
@@ -22,6 +34,7 @@ public class StudentDto {
         this.className = className;
     }
 
+    // Getter and Setter for id
     public long getId() {
         return id;
     }
@@ -30,6 +43,7 @@ public class StudentDto {
         this.id = id;
     }
 
+    // Getter and Setter for name
     public String getName() {
         return name;
     }
